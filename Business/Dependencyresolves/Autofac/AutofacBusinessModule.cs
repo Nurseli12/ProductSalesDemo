63bf89a59f8c994276ccb768e0d4f37be09a6553
NurseliKaya <81693169+Nurseli12@users.noreply.github.com>
2021-08-19 11:50:09 +0300

    19-08

---
+++ Business/Dependencyresolves/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dependencyresolves.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        //This is a IoC field. What does it do? Autofac allows us to keep referans
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Dependencyresolves.Autofac;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +21,16 @@ namespace WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            /* what is this code?
+             * Ýf we want to do diffrence IoC construction. We require to write this code.
+             We are building other IoC constructions with this code
+            Speaking of which what is this other IoC construction ?
+            => Autofac, Ninject, CastleWindsor, StructureMap....*/
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder => 
+                {
+                    builder.RegisterModule(new AutofacBusinessModule());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
